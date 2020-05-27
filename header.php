e68921b104514e50325b8c0dfb4707710da04c4e
Rahul Raju <Rahul@Nikita-Rajus-MacBook-Pro.local>
2020-05-28 00:01:51 +1200

    wordpress formative2

---
+++ header.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<?php wp_head(); ?>
+		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+		<link href="https://fonts.googleapis.com/css2?family=Bebas+Neue&display=swap" rel="stylesheet">
+	</head>
+	<body>
+		<header class="site-header jumbotron jumbotron-fluid">
+		    <div class="container">
+		      <h1><a href="#"><strong>Abundant</strong> Living</a></h1>
+		      <span class="js-search-trigger site-header__search-trigger"><i class="fa fa-search" aria-hidden="true"></i></span>
+		      <i class="site-header__menu-trigger fa fa-bars" aria-hidden="true"></i>
+		      <div class="site-header__menu group">
+		        <nav class="main-navigation">
+		          <ul>
+		            <li><a href="#">About Us</a></li>
+		            <li><a href="#">Programs</a></li>
+		            <li><a href="#">Events</a></li>
+		            <li><a href="#">Campuses</a></li>
+		            <li><a href="#">Blog</a></li>
+		          </ul>
+		        </nav>
+		      </div>
+		    </div>
+		  </header>
